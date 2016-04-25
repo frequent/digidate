34d8e2c36c15fffbefd4e85eb4d96c78dc04b2ab
Sven Franck <svenfranck@yahoo.com>
2016-04-25 18:08:39 +0200

    rename add robot files

---
+++ robot.js
@@ -0,0 +1,83 @@
+/*jslint nomen: true, indent: 2, maxerr: 3 */
+/*global window, rJS */
+(function (window, rJS) {
+  "use strict";
+
+  rJS(window)
+
+    .ready(function (my_gadget) {
+      my_gadget.property_dict = {};
+      return new RSVP.Queue()
+        .push(function () {
+          return my_gadget.getElement();
+        })
+        .push(function (my_element) {
+          my_gadget.property_dict.element = my_element;
+        });
+    })
+
+    .declareMethod('render', function (my_option_dict) {
+      var gadget = this;
+
+      return new RSVP.Queue()
+        .push(function () {
+          return gadget.getDeclaredGadget("robot_gadget");
+        })
+        .push(function (my_robot_gadget) {
+          return my_robot_gadget.render();
+        });
+    })
+
+    .declareMethod('routeStorageRequest', function (my_method, my_param_list) {
+      var gadget = this;
+      return new RSVP.Queue()
+        .push(function () {
+          return gadget.getDeclaredGadget("jio_gadget");
+        })
+        .push(function (my_jio_gadget) {
+          return my_jio_gadget[my_method].apply(
+            my_jio_gadget,
+            [].concat(my_param_list)
+          );
+        })
+        .push(undefined, function (error) {
+          throw error;
+        });
+    })
+
+    // jIO bridge
+    .allowPublicAcquisition("jio_create", function (param_list) {
+      return this.routeStorageRequest("createJIO", param_list);
+    })
+    .allowPublicAcquisition("jio_allDocs", function (param_list) {
+      return this.routeStorageRequest("allDocs", param_list);
+    })
+    .allowPublicAcquisition("jio_remove", function (param_list) {
+      return this.routeStorageRequest("remove", param_list);
+    })
+    .allowPublicAcquisition("jio_post", function (param_list) {
+      return this.routeStorageRequest("post", param_list);
+    })
+    .allowPublicAcquisition("jio_put", function (param_list) {
+      return this.routeStorageRequest("put", param_list);
+    })
+    .allowPublicAcquisition("jio_get", function (param_list) {
+      return this.routeStorageRequest("get", param_list);
+    })
+    .allowPublicAcquisition("jio_allAttachments", function (param_list) {
+      return this.routeStorageRequest("allAttachments", param_list);
+    })
+    .allowPublicAcquisition("jio_getAttachment", function (param_list) {
+      return this.routeStorageRequest("getAttachment", param_list);
+    })
+    .allowPublicAcquisition("jio_putAttachment", function (param_list) {
+      return this.routeStorageRequest("putAttachment", param_list);
+    })
+    .allowPublicAcquisition("jio_removeAttachment", function (param_list) {
+      return this.routeStorageRequest("removeAttachment", param_list);
+    })
+    .allowPublicAcquisition("jio_repair", function (param_list) {
+      return this.routeStorageRequest("repair", param_list);
+    });
+    
+}(window, rJS));
